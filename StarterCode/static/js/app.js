2590b192cd7ce5d372ac3300359e560209028b5c
Victoria A <victoria.arriola@northwestern.edu>
2021-06-09 12:56:53 -0500

    working

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,45 @@
+// function buildPanel(element) {
+//     // clear the panel
+//     // select metadata
+//     // find the metadata number that matches element number
+//     // take metadata and populate panel with what matches (need to run with a number)
+// }
+
+// function buildBubbles(element) {
+//     // select the samples
+//     // find the samples that match the element number that is passed in
+//     // create bar chart
+//     // create bubble chart(same function as bar chart)
+// }
+
+function initialization() {
+    d3.json("samples.json").then((data) => {
+
+}
+    // find names, populate dropdown, 
+    // find first sample name
+
+    // pass first sample call. build bubbles and pass first sample
+
+}
+
+
+
+
+
+
+function optionChanged(element) {
+    buildPanel(element);
+    buildBubbles(element);
+}
+
+initialization();
+
+// create function for dropdown menu change
+
+
+    // bar chart
+
+    
+
+    // bubble chart
