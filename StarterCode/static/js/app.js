6faa7572105e8056dbe8c4f0a86c9a7579eb87a4
Victoria A <victoria.arriola@northwestern.edu>
2021-06-09 21:07:35 -0500

    working

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,19 +1,48 @@
-// function buildPanel(element) {
-//     // clear the panel
-//     // select metadata
-//     // find the metadata number that matches element number
-//     // take metadata and populate panel with what matches (need to run with a number)
-// }
+function buildPanel(element) {
+
+    
+    // clear the panel
+    // select metadata
+    // find the metadata number that matches element number
+    // take metadata and populate panel with what matches (need to run with a number)
+}
 
 // function buildBubbles(element) {
 //     // select the samples
 //     // find the samples that match the element number that is passed in
+
 //     // create bar chart
+    var trace1 = {
+        x:
+        y:
+        text:
+        name:
+        type: "bar"
+        orientation: "h"
+    };
+    var layout_1
+
 //     // create bubble chart(same function as bar chart)
+    var trace2 = {
+        x:
+        y:
+        mode:
+        marker:
+        text:
+    }
+
+    var layout_2 = {
+
+    }
 // }
 
 function initialization() {
+
+    var dropdown = d3.select("#selDataset");
+
     d3.json("samples.json").then((data) => {
+
+        
 
 }
     // find names, populate dropdown, 
@@ -35,11 +64,3 @@ function optionChanged(element) {
 
 initialization();
 
-// create function for dropdown menu change
-
-
-    // bar chart
-
-    
-
-    // bubble chart
