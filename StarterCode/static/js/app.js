e5f8be0d699e9c5d9063288db0615f4f3810e238
Victoria A <victoria.arriola@northwestern.edu>
2021-06-09 23:31:00 -0500

    working

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,56 +1,84 @@
 function buildPanel(element) {
+    // select metadata
+    d3.json("samples.json").then((data) => {
+        var metadata = data.metadata;
+
+        var result = metadata.filter(subject => subject.id === element)[0];
+        // clear the panel
+    var demoPanel = d3.select("#sample-metadata");
+    demoPanel.html("");
 
     
-    // clear the panel
-    // select metadata
+    
+    Object.entries(result[0]).forEach(subject => {
+        demoPanel.append("h5").text(`${subject[0]}: ${subject[1]}`);
+
+    });
+
+    });
+
+
+
+}
+    
+
+
     // find the metadata number that matches element number
     // take metadata and populate panel with what matches (need to run with a number)
-}
 
-// function buildBubbles(element) {
-//     // select the samples
-//     // find the samples that match the element number that is passed in
 
-//     // create bar chart
-    var trace1 = {
-        x:
-        y:
-        text:
-        name:
-        type: "bar"
-        orientation: "h"
-    };
-    var layout_1
+function buildBubbles(element) {
+// //     // select the samples
+// //     // find the samples that match the element number that is passed in
 
-//     // create bubble chart(same function as bar chart)
-    var trace2 = {
-        x:
-        y:
-        mode:
-        marker:
-        text:
-    }
+// //     // create bar chart
+//     var trace1 = {
+//         x:
+//         y:
+//         text:
+//         name:
+//         type: "bar"
+//         orientation: "h"
+//     };
+//     var layout_1
 
-    var layout_2 = {
+// //     // create bubble chart(same function as bar chart)
+//     var trace2 = {
+//         x:
+//         y:
+//         mode:
+//         marker:
+//         text:
+//     }
 
-    }
-// }
+//     var layout_2 = {
+
+//     }
+// // }
 
 function initialization() {
-
+    // find names, populate dropdown, 
     var dropdown = d3.select("#selDataset");
+
 
     d3.json("samples.json").then((data) => {
 
-        
+        var sampleNames = data.names;
 
-}
-    // find names, populate dropdown, 
-    // find first sample name
-
+        sampleNames.forEach(function(name) {
+            dropdown.append("option").text(name).property("value");
+        });
+    
     // pass first sample call. build bubbles and pass first sample
+    
+        const firstSubject = sampleNames[0];
+        buildBubbles(firstSubject);
+        buildPanel(firstSubject);
+});
 
 }
+
+    // find first sample name
 
 
 
